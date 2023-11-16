52e6a2f6c5b6482d5eaa8329684eda062c809695
UlanZhassanov <ulanzhasanov@gmail.com>
2023-11-16 12:16:45 +0600

    add, update employee

--- src/main/java/com/ulanzhassanov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/ulanzhassanov/spring/rest/controller/MyRESTController.java
@@ -37,4 +37,20 @@ public class MyRESTController {
         return employee;
     }
 
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
 }
